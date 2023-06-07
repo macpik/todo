a8c355c6d5de7f86221702e78d9c51c81e528f02
Maciek Pikula <maciej.pikula@icloud.com>
2023-06-07 14:46:56 +0100

    add structure

---
+++ todos.py
@@ -0,0 +1,12 @@
+todos = [
+    {
+        'title': 'Zakupy',
+        'description': 'Mleko, jajka, mąka, olej, papier toaletowy (jak będzie)',
+        'done': False
+    },
+    {
+        'title': 'Zrobić zadania z Pythona',
+        'description': 'Przygotować projekt z modułu i wysłać Mentorowi',
+        'done': False
+    }
+]
